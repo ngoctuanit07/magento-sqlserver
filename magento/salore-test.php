0901ab5739bdc31ad1403ce5f935ea37b417a23b
Nguyễn Tuân <ngoctuanit07@gmail.com>
2014-12-20 19:07:17 +0700

    testing

--- magento/salore-test.php
+++ magento/salore-test.php
@@ -4,5 +4,6 @@ Mage::app();
 Mage::setIsDeveloperMode(true);
 ini_set('display_errors', 1);
 $connection = Mage::getSingleton('core/resource')->getConnection('sbmssql_write');
-$result = $connection->select()->from('tblItem');
-print_r($result); die();
+$select = $connection->select()->from('tblItem');
+$smt = $connection->fetchAll($select);
+print_r($smt); die();
