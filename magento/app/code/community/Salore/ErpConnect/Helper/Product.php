38338914c59d377758aa0881d494555842415f08
Nguyễn Tuân <ngoctuanit07@gmail.com>
2014-12-20 19:34:35 +0700

    testing import data

--- magento/app/code/community/Salore/ErpConnect/Helper/Product.php
+++ magento/app/code/community/Salore/ErpConnect/Helper/Product.php
@@ -25,13 +25,37 @@ class Salore_ErpConnect_Helper_Product extends Mage_Core_Helper_Abstract {
 	}
 	public function createProduct($data) {
 		$product = Mage::getModel('catalog/product');
-		$product->setData('sku' , $data['SKU']);
-		$product->setData('name' , $data['SKU_Name']);
-		$product->setData('short_description' , $data['ShortDesc']);
-		$product->setData('description' , $data['ShortDesc']);
-		$product->setData('price' , $data['Price']);
-		$product->setData('qty' , $data['Qty']);
-		$product->save();
+		try {
+			$product
+			->setWebsiteIds(array(1)) //website ID the product is assigned to, as an array
+			->setAttributeSetId(9) //ID of a attribute set named 'default'
+			->setTypeId('simple') //product type
+			->setCreatedAt(strtotime('now')) //product creation time
+			->setSku($data['SKU']) //SKU
+			->setName($data['SKU_Name']) //product name
+			->setWeight(4.0000)
+			->setStatus(1) //product status (1 - enabled, 2 - disabled)
+			->setTaxClassId(4) //tax class (0 - none, 1 - default, 2 - taxable, 4 - shipping)
+			->setVisibility(Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH) //catalog and search visibility
+			->setNewsToDate('06/30/2014') //product set as new to
+			->setCountryOfManufacture('AF') //country of manufacture (2-letter country code)
+			->setPrice($data['Price']) //price in form 11.22
+			->setDescription($data['ShortDesc'])
+			->setShortDescription($data['ShortDesc'])
+			->setMediaGallery (array('images'=>array (), 'values'=>array ())) //media gallery initialization
+			->setStockData(array(
+					'use_config_manage_stock' => 1, //'Use config settings' checkbox
+					'manage_stock'=>1, //manage stock
+					'min_sale_qty'=>1, //Minimum Qty Allowed in Shopping Cart
+					'max_sale_qty'=>2, //Maximum Qty Allowed in Shopping Cart
+					'is_in_stock' => 1, //Stock Availability
+					'qty' => $data['Qty'] //qty
+			)
+			)
+			$product->save();
+		} catch (Exception $e) {
+			throw $e;
+		}
 	}
 	
 }
