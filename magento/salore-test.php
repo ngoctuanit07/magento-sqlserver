c663cca2b2ab6473dc34199d835e30db4201c40f
Nguyễn Tuân <ngoctuanit07@gmail.com>
2014-12-20 18:16:52 +0700

    Testing

--- magento/salore-test.php
+++ magento/salore-test.php
@@ -5,7 +5,7 @@ Mage::setIsDeveloperMode(true);
 ini_set('display_errors', 1);
 //$connection = Mage::getSingleton('core/resource')->getConnection('sbmssql_write');
 $connection = new Zend_Db_Adapter_Pdo_Mssql(array(
-		'host'     => '192.168.1.211 , 1433', // parklife
+		'host'     => '192.168.1.211', // parklife
 		'username' => 'sa',
 		'password' => 'qaz123456',
 		'dbname'   => 'Sage',
