41ffc3a40323504c5623406a1401150375257fb5
Nguyễn Tuân <ngoctuanit07@gmail.com>
2014-12-20 17:58:11 +0700

    testing adapter mssql

--- magento/salore-test.php
+++ magento/salore-test.php
@@ -1,6 +1,8 @@
 <?php
 require_once ('app/Mage.php');
 Mage::app();
+Mage::setIsDeveloperMode(true);
+ini_set('display_errors', 1);
 $connection = Mage::getSingleton('core/resource')->getConnection('sbmssql_write');
 $result = $connection->getServerVersion();
 print_r($result); die();
