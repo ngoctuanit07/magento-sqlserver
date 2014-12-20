3c5b8d1981505cc5bf35466895d39acde91d1844
Nguyễn Tuân <ngoctuanit07@gmail.com>
2014-12-20 18:35:27 +0700

    refactor salore adapter mssql

--- magento/app/code/community/Salore/ErpConnect/Helper/Data.php
+++ magento/app/code/community/Salore/ErpConnect/Helper/Data.php
@@ -10,11 +10,14 @@
  * @author      Salore team
  * @copyright   Copyright (c) Salore team
  */
-class Salore_ErpConnect_Helper_Data extends Salore_Sqlsrv_Helper_Data {
+class Salore_ErpConnect_Helper_Data extends Mage_Core_Helper_Abstract {
 	public function getAddressField($bind , $field) {
 		if (isset($bind[$field])) {
 			return $bind[$field];
 		}
 		return null;
+	}
+	public function getConnection() {
+		return Mage::getModel('core/resource')->getConnection('sbmssql_write');
 	}
 }
--- magento/salore-test.php
+++ magento/salore-test.php
@@ -3,13 +3,6 @@ require_once ('app/Mage.php');
 Mage::app();
 Mage::setIsDeveloperMode(true);
 ini_set('display_errors', 1);
-//$connection = Mage::getSingleton('core/resource')->getConnection('sbmssql_write');
-$connection = new Zend_Db_Adapter_Pdo_Mssql(array(
-		'host'     => '192.168.1.211', // parklife
-		'username' => 'sa',
-		'password' => 'qaz123456',
-		'dbname'   => 'Sage',
-		'pdoType'  =>  'dblib' )
-);
+$connection = Mage::getSingleton('core/resource')->getConnection('sbmssql_write');
 $result = $connection->getServerVersion();
 print_r($result); die();
