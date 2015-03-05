e85cb85d7aa759351be3f4114271b17b5b09f918
tuan <tuan@TuanUbuntu>
2015-03-06 01:54:50 +0700

    refresh

--- magento/app/code/community/Salore/Mssql/Adapter/Mssql.php
+++ magento/app/code/community/Salore/Mssql/Adapter/Mssql.php
@@ -11,5 +11,13 @@
  * @copyright   Copyright (c) Salore team
  */
 class Salore_Mssql_Adapter_Mssql extends Salore_Mssql_Adapter_Abstract {
-		
+	public function __construct($config) {
+		//Override config values from admin settings
+		$configs = Mage::getStoreConfig('sbmssql/setting');
+		if(isset($configs['password']) && !empty($configs['password'])) {
+			$configs['password'] = Mage::helper('core')->decrypt( $configs['password'] );
+		}
+		$this->_config = array_merge($config , $configs);
+		parent::__construct($this->_config);
+	}
 }
